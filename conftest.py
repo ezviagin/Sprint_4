90af33682a23d3bcd0388febc8db8664e1b7e44b
Stas Basov <egor.zviagintcev@yandex.ru>
2024-04-22 12:36:38 +0300

    Changes after review #2

--- conftest.py
+++ conftest.py
@@ -3,11 +3,6 @@ from main import BooksCollector
 
 
 @pytest.fixture(scope="class")
-def books_collector():
-    return BooksCollector()
-
-
-@pytest.fixture(scope="class")
 def expected_genre():
     return ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
 
@@ -17,8 +12,14 @@ def expected_genre_18_plus():
     return ['Ужасы', 'Детективы']
 
 
-@pytest.fixture(scope="class")
-def filled_book_collection(books_collector):
+@pytest.fixture(scope="function")
+def books_collector():
+    return BooksCollector()
+
+
+@pytest.fixture(scope="function")
+def filled_book_collection():
+    books_collector = BooksCollector()
     books_data = [
         ['Мертвая зона', 'Ужасы'],
         ['Шерлок Холмс', 'Детективы'],
@@ -28,4 +29,5 @@ def filled_book_collection(books_collector):
     for book_name, genre in books_data:
         books_collector.add_new_book(book_name)
         books_collector.set_book_genre(book_name, genre)
+
     return books_collector
--- test.py
+++ test.py
@@ -9,22 +9,23 @@ class TestBooksCollector:
     def test_favorites_init_empty(self, books_collector):
         assert books_collector.favorites == []
 
-    def test_genre_init_correctly(self, expected_genre, books_collector):
+    def test_genre_init_correctly(self, books_collector, expected_genre, ):
         assert books_collector.genre == expected_genre
 
-    def test_genre_age_rating_init_correctly(self, expected_genre_18_plus, books_collector):
+    def test_genre_age_rating_init_correctly(self, books_collector, expected_genre_18_plus):
         assert books_collector.genre_age_rating == expected_genre_18_plus
 
-    @pytest.mark.parametrize("book_name, expected_result", [
+    @pytest.mark.parametrize('book_name, expected_result', [
         ('Властелин Колец', True),
-        ("Очень длинное название, которое не должно добавиться в качестве названия книги", False)
+        ('Очень длинное название, которое не должно добавиться в качестве названия книги', False)
     ])
-    def test_add_new_book_orig_and_less_than_41_symbol(self, book_name, expected_result, books_collector):
+    def test_add_new_book_orig_and_less_than_41_symbol(self, books_collector, book_name, expected_result):
         books_collector.add_new_book(book_name)
-        assert (book_name in books_collector.books_genre) == expected_result
+        names = books_collector.get_books_genre()
+        assert (book_name in names) == expected_result
 
     @pytest.mark.parametrize("book_name, expected_result", [('Властелин Колец', '')])
-    def test_add_new_book_genre_is_empty(self, book_name, expected_result, books_collector):
+    def test_add_new_book_genre_is_empty(self, books_collector, book_name, expected_result):
         books_collector.add_new_book(book_name)
         assert books_collector.get_book_genre(book_name) == expected_result
 
@@ -36,16 +37,10 @@ class TestBooksCollector:
         assert books_collector.get_book_genre(book_name) == book_name_genre
 
     @pytest.mark.parametrize('book_name, book_name_genre', [('Властелин Колец', 'Фантастика')])
-    def test_set_book_genre_correct(self, books_collector, book_name, book_name_genre):
+    def test_set_and_get_book_genre_correct(self, books_collector, book_name, book_name_genre):
         books_collector.add_new_book(book_name)
         books_collector.set_book_genre(book_name, book_name_genre)
         assert books_collector.get_book_genre(book_name) == book_name_genre
-
-    @pytest.mark.parametrize('book_name, book_name_genre', [('Властелин Колец', 'Фантастика')])
-    def test_get_book_genre_correct(self, filled_book_collection, book_name, book_name_genre):
-        filled_book_collection.add_new_book(book_name)
-        filled_book_collection.set_book_genre(book_name, book_name_genre)
-        assert filled_book_collection.get_book_genre(book_name) == book_name_genre
 
     def test_get_books_for_children_no_adult_genres(self, filled_book_collection, expected_genre_18_plus):
         children_books = filled_book_collection.get_books_for_children()
@@ -54,23 +49,21 @@ class TestBooksCollector:
     def test_add_book_in_favorites_existing_book_success(self, filled_book_collection, book_favorite='Гарри Поттер и Узник Азкабана'):
         filled_book_collection.add_new_book(book_favorite)
         filled_book_collection.add_book_in_favorites(book_favorite)
-        assert book_favorite in filled_book_collection.favorites
+        assert book_favorite in filled_book_collection.get_list_of_favorites_books()
 
-    def test_add_book_in_favorites_non_existing_book_failed(self, filled_book_collection):
+    def test_add_book_in_favorites_non_existing_book_skip(self, filled_book_collection):
         book_name = 'Нет такой книги'
         filled_book_collection.add_book_in_favorites(book_name)
-        assert book_name not in filled_book_collection.favorites
+        assert book_name not in filled_book_collection.get_list_of_favorites_books()
 
-    @pytest.mark.parametrize('book_name', [('Волшебник изумрудного города', 'Нет такой книги')])
-    def test_delete_book_from_favorites_existing_book_success(self, filled_book_collection, book_name):
+    @pytest.mark.parametrize('book_name', ['Волшебник изумрудного города'])
+    def test_delete_book_from_favorites_existing(self, filled_book_collection, book_name):
         filled_book_collection.add_new_book(book_name)
         filled_book_collection.add_book_in_favorites(book_name)
         filled_book_collection.delete_book_from_favorites(book_name)
-        assert book_name not in filled_book_collection.favorites
+        assert book_name not in filled_book_collection.get_list_of_favorites_books()
 
-    @pytest.mark.parametrize('book_name', [('Волшебник изумрудного города', 'Нет такой книги')])
-    def test_delete_book_from_favorites_non_existing_book_failed(self, filled_book_collection, book_name):
-        filled_book_collection.add_new_book(book_name)
-        filled_book_collection.add_book_in_favorites(book_name)
+    @pytest.mark.parametrize('book_name', ['Нет такой книги'])
+    def test_delete_book_from_favorites_non_existing(self, filled_book_collection, book_name):
         filled_book_collection.delete_book_from_favorites(book_name)
-        assert book_name not in filled_book_collection.favorites
+        assert book_name not in filled_book_collection.get_list_of_favorites_books()
